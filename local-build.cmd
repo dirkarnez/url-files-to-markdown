af715e7817c7dbc74d408b6dc05d8add9dbabe18
Dirk Arnez <smalldirkalex@gmail.com>
2025-10-06 14:54:39 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -1,11 +1,20 @@
 @echo off
 
-set GOROOT=%USERPROFILE%\Downloads\go1.21.0.windows-amd64\go
-set GOPATH=%USERPROFILE%\Downloads\gopath
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+
+set GOROOT=%DOWNLOADS_DIR%\go1.21.0.windows-amd64\go
+set GOPATH=%DOWNLOADS_DIR%\gopath
 
 set PATH=^
-%USERPROFILE%\Downloads\PortableGit\bin;^
+%WINDIR%\System32;^
 %GOROOT%\bin;
+
+SET PATH=^
+%PATH%;^
+%DOWNLOADS_DIR%\PortableGit\bin;^
+%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64;^
+%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin;
 
 go build main.go &&^
 pause
+
